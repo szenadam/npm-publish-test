2cccaf6c9ebca9cc90b183f3b4339759e75bb8bd
szenadam <szenadam1@gmail.com>
2020-04-08 17:08:32 +0200

    Add logs to functions, bump beta version

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function add(a, b) {
-    return a + b;
+  console.log("foo");
+  return a + b;
 }
 
 function sub(a, b) {
-    return a - b;
+  console.log("bar");
+  return a - b;
 }
 
 function multi(a, b) {
-    return a * b;
+  console.log("baz");
+  return a * b;
 }
 
 module.exports = { add, sub, multi };
